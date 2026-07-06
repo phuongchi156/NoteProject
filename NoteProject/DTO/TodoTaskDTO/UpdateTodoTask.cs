d8ea6eed7e089f485fb1fab37cb70d99d52ff5a3
phuongchi156 <phuongchiokita@gmail.com>
2026-07-06 14:37:40 +0700

    todoTask api

---
+++ NoteProject/DTO/TodoTaskDTO/UpdateTodoTask.cs
@@ -0,0 +1,11 @@
+﻿namespace NoteProject.DTO.TodoTaskDTO
+{
+    public class UpdateTodoTask
+    {
+        public string Title { get; set; } = null!;
+        public string? Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool IsCompleted { get; set; } = false;
+        public int Priority { get; set; } //1 = hight, 2 = medium, 3 = low
+    }
+}
--- NoteProject/Models/TodoTask.cs
+++ NoteProject/Models/TodoTask.cs
@@ -4,13 +4,13 @@ using System;
 
 namespace NoteProject.Models;
 
-public class Tasks
+public class TodoTask
 {
     public Guid Id { get; set; }
     public string Title { get; set; } = null!;
     public string? Description { get; set; }
     public DateTime DueDate { get; set; }
-    public bool IsCompleted { get; set; }
+    public bool IsCompleted { get; set; } = false;
     public Guid UserId { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.Now;
     public DateTime UpdatedAt { get; set; } = DateTime.Now;
